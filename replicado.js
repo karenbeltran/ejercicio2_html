c98192e8557adbf512ac2e54f833ff7b24b72b79
karenbeltran <beltranramirezkaren@gmail.com>
2017-03-16 15:17:08 +0000

    ejercicio2_html

---
+++ replicado.js
@@ -0,0 +1,29 @@
+<<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+<body>
+	<h1>Grace  Hopper </h1>
+	<h2>Bio</h2>
+	<p> Grace Murray Hopper nacio en Nueva York el 9 de Diciembre de 1906 en el seno de una familia en la que los consideraban que los varones y las mueres debian tener las mismas oportunidades 
+	</p>
+	<ul>
+		<li> cocinar </li>
+		<li> progamar </li>
+	<ul>JavaScrip</ul>
+		<li>Ruby</li>
+		<li>Java</li>
+		<li>Enseñar</li>
+  <h3> 3 Sueños que tengo </h3>
+  	<ol>
+  		<li>viajar po el mundo <br>
+  		<li> Ver a Laboratoria por todo el mundo </li> <br>
+  		<li>Que todos aprendan  Codear </div>
+	
+		<img src="images.jpg">
+
+
+
+</body>
+</html>
